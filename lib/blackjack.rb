73f3d78e417ccdbe27ecd94fc0317b052af3bce4
Skye Geraghty <skyetay03@googlemail.com>
2020-02-13 11:12:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -70,7 +70,7 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  while card_total < 21 do
+  while card_total <= 21 do
     card_total = hit?(card_total)
   end
   end_game(card_total)
